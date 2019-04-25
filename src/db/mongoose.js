3977a84e96cc58fb0417dd7b85f6893f8107e31e
Ben Chi <benthechi@gmail.com>
2019-04-24 17:30:51 -0700

    Changed mongodb url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+mongoose.connect('hyperkitty-pv4q1.mongodb.net:27017',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
